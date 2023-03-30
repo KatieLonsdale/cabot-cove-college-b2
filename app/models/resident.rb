4fc9ddf7cf5b90fbdf1930c21c60218cf27c9c30
Katie Lonsdale <katie952@gmail.com>
2023-03-29 19:44:41 -0600

    feat: add average age method

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -6,6 +6,10 @@ class Resident < ApplicationRecord
   has_many :resident_courses
   has_many :courses, through: :resident_courses
 
+  def self.average_age
+    average(:age).to_f.round(1)
+  end
+
   def list_courses
     courses = ResidentCourse.select(:course_id).where(resident_id: id)
     Course.where(id: courses)
