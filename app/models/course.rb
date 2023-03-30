6d4a26b2fa8ac66949950987272c3772bf8bf355
Katie Lonsdale <katie952@gmail.com>
2023-03-29 19:10:43 -0600

    feat: add number_enrolled method

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ApplicationRecord
   validates_presence_of :name
   has_many :resident_courses
   has_many :residents, through: :resident_courses
+
+  def number_enrolled
+    ResidentCourse.select(:resident_id).where(course_id: id).size
+  end
 end
