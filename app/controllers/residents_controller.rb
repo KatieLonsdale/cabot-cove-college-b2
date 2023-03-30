0760d780a7b478b91df27b82d02a8324a61ef604
Katie Lonsdale <katie952@gmail.com>
2023-03-29 18:47:32 -0600

    feat: add show action

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -2,4 +2,9 @@ class ResidentsController < ApplicationController
   def index
     @residents = Resident.all
   end
+
+  def show
+    @resident = Resident.find(params[:resident_id])
+    @courses = @resident.list_courses
+  end
 end
