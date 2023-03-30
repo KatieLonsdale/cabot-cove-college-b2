edb58f03d2fb697f942db7e4e22e0d5cc32cbbf5
Katie Lonsdale <katie952@gmail.com>
2023-03-29 19:26:38 -0600

    fix: list_courses method

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -7,7 +7,7 @@ class Resident < ApplicationRecord
   has_many :courses, through: :resident_courses
 
   def list_courses
-    courses = ResidentCourse.where(resident_id: id)
+    courses = ResidentCourse.select(:course_id).where(resident_id: id)
     Course.where(id: courses)
   end
 end
